445fd4fa7e2df1cd44596d808713b78cd6b93b0e
rayanses <thefastestmaninthelife@gmail.com>
2025-09-16 23:34:49 +0300

    modified:   dom/main.js

--- dom/main.js
+++ dom/main.js
@@ -1,3 +1,9 @@
 let header = document.createElement("header");
 let logo = document.createElement("span");
 logo.textContent="Elzero";
+header.appendChild(logo);
+
+let ul = document.createElement("ul");
+let li1 = document.createElement("li");
+li1.textContent="Home";
+let li2 = document.createElement("li");
