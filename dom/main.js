85b7da6d5abd539d6a5f3381793e677ce7765611
rayanses <thefastestmaninthelife@gmail.com>
2025-09-17 00:04:52 +0300

    modified:   dom/main.js

--- dom/main.js
+++ dom/main.js
@@ -23,4 +23,24 @@ document.body.appendChild(header);
 header.style.cssText="display:flex;justify-content:space-between;align-items:center;padding:20px;font-family:Arial, sans-serif";
 logo.style.cssText="color:#23a96e;font-weight:bold;font-size:26px;";
 ul.style.cssText="list-style:none;display:flex;gap:20px;margin:0;padding:0";
-let 
+
+let grid = document.createElement("div");
+grid.className="grid";
+document.body.appendChild(grid);
+grid.style.cssText="background-color: #eaebecff ;display:grid;grid-template-columns:repeat(3,1fr);gap:20px;padding:20px;font-family:Arial, sans-serif";
+for (let i=1;i<=15;i++){
+    let product = document.createElement("div");
+    product.className="product"; 
+    let pnum = document.createElement("span");
+    pnum.textContent=i;
+
+    product.appendChild(pnum);
+
+    product.style.cssText="background-color:#f7f7f7;border:1px solid #ddd;border-radius:5px;padding:20px;text-align:center;";
+
+    grid.appendChild(product);
+    pnum.style.cssText="display:block;font-size:25px;color:#23a96e;margin-bottom:10px;";
+    
+}
+
+    document.body.appendChild(product);
