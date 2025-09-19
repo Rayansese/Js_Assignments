c82564ca1f5b1d8665a10ad1e22673a5e0c124e8
rayanses <thefastestmaninthelife@gmail.com>
2025-09-19 12:34:11 +0300

    new file:   bom/1.html 	new file:   bom/1.js 	modified:   dom/main.js

---
+++ bom/1.js
@@ -0,0 +1,2 @@
+let prompt = window.prompt("Print Number From - To","Example: 5-20");
+console.log(prompt);
--- dom/main.js
+++ dom/main.js
@@ -1,55 +1,60 @@
 let header = document.createElement("header");
 let logo = document.createElement("span");
-logo.textContent="Elzero";
+logo.textContent = "Elzero";
 header.appendChild(logo);
 
 let ul = document.createElement("ul");
 let li1 = document.createElement("li");
-li1.textContent="Home";
+li1.textContent = "Home";
 let li2 = document.createElement("li");
-li2.textContent="About";
+li2.textContent = "About";
 let li3 = document.createElement("li");
-li3.textContent="Service";
+li3.textContent = "Service";
 let li4 = document.createElement("li");
-li4.textContent="Contact";
+li4.textContent = "Contact";
 ul.appendChild(li1);
 ul.appendChild(li2);
 ul.appendChild(li3);
 ul.appendChild(li4);
 header.appendChild(ul);
-// how to make css to those ?   
+// how to make css to those ?
 document.body.appendChild(header);
 
-header.style.cssText="display:flex;justify-content:space-between;align-items:center;padding:20px;font-family:Arial, sans-serif";
-logo.style.cssText="color:#23a96e;font-weight:bold;font-size:26px;";
-ul.style.cssText="list-style:none;display:flex;gap:20px;margin:0;padding:0";
+header.style.cssText =
+  "display:flex;justify-content:space-between;align-items:center;padding:20px;font-family:Arial, sans-serif";
+logo.style.cssText = "color:#23a96e;font-weight:bold;font-size:26px;";
+ul.style.cssText = "list-style:none;display:flex;gap:20px;margin:0;padding:0";
 
 let grid = document.createElement("div");
-grid.className="grid";
+grid.className = "grid";
 document.body.appendChild(grid);
-grid.style.cssText="background-color: #eaebecff ;display:grid;grid-template-columns:repeat(3,1fr);gap:20px;padding:20px;font-family:Arial, sans-serif";
-for (let i=1;i<=15;i++){
-    let product = document.createElement("div");
-    product.className="product"; 
-    let pnum = document.createElement("span");
-    pnum.textContent=i;
+grid.style.cssText =
+  "background-color: #eaebecff ;display:grid;grid-template-columns:repeat(3,1fr);gap:20px;padding:20px;font-family:Arial, sans-serif";
+for (let i = 1; i <= 15; i++) {
+  let product = document.createElement("div");
+  product.className = "product";
+  let pnum = document.createElement("span");
+  pnum.textContent = i;
 
-    product.appendChild(pnum);
+  product.appendChild(pnum);
 
-    product.style.cssText="background-color:#f7f7f7;border:1px solid #ddd;border-radius:5px;padding:20px;text-align:center;";
+  product.style.cssText =
+    "background-color:#f7f7f7;border:1px solid #ddd;border-radius:5px;padding:20px;text-align:center;";
 
-    grid.appendChild(product);
-    pnum.style.cssText="display:block;font-size:25px;color:#23a96e;margin-bottom:10px;";
-    let ptext = document.createElement("span");
-    ptext.textContent="Product";
-    product.appendChild(ptext);
-    ptext.style.cssText="display:block;font-size:15px;color:#888;";
+  grid.appendChild(product);
+  pnum.style.cssText =
+    "display:block;font-size:25px;color:#23a96e;margin-bottom:10px;";
+  let ptext = document.createElement("span");
+  ptext.textContent = "Product";
+  product.appendChild(ptext);
+  ptext.style.cssText = "display:block;font-size:15px;color:#888;";
 }
 
-    document.body.appendChild(product);
-    let feeter = document.createElement("div");
-    feeter.textContent="Copyright 2024";
-    document.body.appendChild(feeter);
-    feeter.style.cssText="background-color:#23a96e;color:white;text-align:center;padding:20px;font-family:Arial, sans-serif;margin-top:20px; position:fixed;bottom:0;width:100%";
-    document.body.style.cssText="margin:0;padding:0;background-color:#eaebecff;";
-    document.title="Document ";
+let footer = document.createElement("footer");
+footer.textContent = "Copyright 2024";
+document.body.appendChild(footer);
+footer.style.cssText =
+  "background-color:#23a96e;color:white;text-align:center;padding:20px;font-family:Arial, sans-serif;margin-top:20px; position:fixed;bottom:0;width:100%";
+document.body.style.cssText = "margin:0;padding:0;background-color:#eaebecff;";
+document.title = "Document ";
+console.log(footer);
